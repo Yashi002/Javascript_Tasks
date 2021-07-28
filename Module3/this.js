505fb90628ddd83caa512102274a09cbb676c175
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-07-28 22:25:09 +0530

    Add files via upload

---
+++ Module3/this.js
@@ -0,0 +1,13 @@
+// method
+
+const video = {
+  title: 'a',
+  play() {
+    console.log(this);
+  }
+};
+function Video(title) {
+  this.title = title;
+  console.log(this);
+}
+const v = new Video('b');
